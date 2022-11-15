fd87fc4f6c07adf977788ab747a619e9752924a7
Harshit <harshitsingha0@gmail.com>
2022-11-15 12:02:55 +0530

    check

--- src/AmeerBank.java
+++ src/AmeerBank.java
@@ -1,4 +1,39 @@
 package src;
 
-public class AmeerBank {
+public class AmeerBank implements BankingSystem {
+    int accountNo;
+    String Name;
+    String PAN;
+    int balance;
+    int year;
+    int InterestRate = 10;
+
+    public AmeerBank(int accountNo, String name, String PAN, int balance, int year, int interestRate) {
+        this.accountNo = accountNo;
+        Name = name;
+        this.PAN = PAN;
+        this.balance = balance;
+        this.year = year;
+        InterestRate = interestRate;
+    }
+
+    @Override
+    public void AccBalance() {
+
+    }
+
+    @Override
+    public void WithdrawMoney(int money) {
+
+    }
+
+    @Override
+    public void deposit(int money) {
+
+    }
+
+    @Override
+    public void calculateEMI(int balance) {
+
+    }
 }
